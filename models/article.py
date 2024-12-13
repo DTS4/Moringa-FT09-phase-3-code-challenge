bcdecf59c7f20d20f135586d140760336269c46e
Keith <keith.githinji@student.moringaschool.com>
2024-12-13 20:58:20 +0300

    Added the Article class with validation for author, magazine, and title, and functionality to insert articles into the database.

--- models/article.py
+++ models/article.py
@@ -1,10 +1,44 @@
+from database.connection import get_db_connection
+from models.author import Author
+from models.magazine import Magazine
+
 class Article:
-    def __init__(self, id, title, content, author_id, magazine_id):
-        self.id = id
+    def __init__(self, author, magazine, title):
+        if not isinstance(author, Author):
+            print("Author must be an instance of Author")
+            return
+        if not isinstance(magazine, Magazine):
+            print("Magazine must be an instance of Magazine")
+            return
+        if not isinstance(title, str) or not (5 <= len(title) <= 50):
+            print("Title must be a string between 5 and 50 characters")
+            return
+
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute(
+            'INSERT INTO articles (title, content, author_id, magazine_id) VALUES (?, ?, ?, ?)',
+            (title, '', author.id, magazine.id)
+        )
+        conn.commit()
+        self.id = cursor.lastrowid
+        conn.close()
+
+        self.author = author
+        self.magazine = magazine
         self.title = title
-        self.content = content
-        self.author_id = author_id
-        self.magazine_id = magazine_id
 
     def __repr__(self):
         return f'<Article {self.title}>'
+
+    @property
+    def author(self):
+        return self._author
+
+    @property
+    def magazine(self):
+        return self._magazine
+
+    @property
+    def title(self):
+        return self._title
