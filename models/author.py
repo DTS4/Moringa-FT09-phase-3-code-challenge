479413641722f3489d54001295a87ab9461d44d8
Keith <keith.githinji@student.moringaschool.com>
2024-12-13 21:04:19 +0300

    Implemented the Author class with database insertion and a property to retrieve articles associated with the author.

--- models/author.py
+++ models/author.py
@@ -1,7 +1,24 @@
+from database.connection import get_db_connection
+
 class Author:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        conn = get_db_connection()
+        cursor = conn.cursor()
+
+        cursor.execute('INSERT OR IGNORE INTO authors (id, name) VALUES (?, ?)', (id, name))
+        conn.commit()
+        conn.close()
 
     def __repr__(self):
         return f'<Author {self.name}>'
+
+    @property
+    def articles(self):
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM articles WHERE author_id = ?', (self.id,))
+        articles = cursor.fetchall()
+        conn.close()
+        return [article(article['id'], article['title'], article['content'], article['author_id'], article['magazine_id']) for article in articles]
